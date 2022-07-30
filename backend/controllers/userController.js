269aa30a1eafb9301e6a2a9cf37d54501eca6595
Michael Sell <michael.sell@usa.net>
2022-07-30 10:53:51 +0200

    Register and Login with Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
 // Allows to use async/await in the controller
 const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
 
 // @desc  - Register a new user
 // @route - POST /api/users
@@ -13,15 +16,64 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Please fill in all fields');
   }
-  res.send('Register Route');
+  //Check if user already exists:
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  //Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res
+      .status(201)
+      .json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+  } else {
+    res.status(400);
+    throw new Error('User could not be created');
+  }
 });
 
 // @desc - Login a user
 // @route - POST /api/users/login
 // @access -
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route');
+  // Get the email and password from the request body
+  const { email, password } = req.body;
+  // Find the user by email
+  const user = await User.findOne({ email });
+  // if the user exists and the hashed password matches the password in the database
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
 });
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 module.exports = {
   registerUser,
---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide a name.'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide an email.'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password.'],
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
