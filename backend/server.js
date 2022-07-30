4901186c29bcb300bffef4ff4be203961021e9cf
Michael Sell <michael.sell@usa.net>
2022-07-30 06:48:29 +0200

    Server setup and generic Route installed

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server ...');
+const express = require('express');
+
+// Get the environment variables from .env
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+// Initialization
+const app = express();
+
+// Define Routes
+app.get('/', (req, res) => {
+  // res.send('Hello');
+  res.status(201).json({ message: 'Welcome to the Support Desk API' });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
